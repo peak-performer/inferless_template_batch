cfe219021b8d02c52127c314889d267dd574391b
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2023-05-15 19:47:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ class InferlessPythonModel:
         pipeline_output = self.generator(prompt, do_sample=True, min_length=20)
         generated_txt = pipeline_output[0]["generated_text"]
         print("auto build testing", flush=True)
-        return {"generated_text": generated_txt}
+        return {"generated_text": "testing"}
 
     # perform any cleanup activity here
     def finalize(self,args):
